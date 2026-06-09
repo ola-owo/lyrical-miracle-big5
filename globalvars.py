652d27794a65409793d4a05b1ab7897932b24516
Ola <ola.owoputi@gmail.com>
2026-06-08 23:30:36 -0400

    init

---
+++ globalvars.py
@@ -0,0 +1,17 @@
+HF_USER = 'ola-owo'
+HF_DATASET = "ola-owo/big-five-personality-traits"
+
+BASE_MODEL_NAME = 'distilbert-base-multilingual-cased'
+BASE_MODEL = f'distilbert/{BASE_MODEL_NAME}'
+BASE_MODEL_ARCH = 'DistilBertModel'
+
+MODEL_NAME = 'distilbert-bigfive-sentence-transformer'
+LORA_MODEL_NAME = MODEL_NAME + '-lora'
+MODEL = f'{HF_USER}/{MODEL_NAME}'
+LORA_MODEL = f'{HF_USER}/{LORA_MODEL_NAME}'
+
+WANDB_PROJECT = "LyricalMiracle"
+
+BIG5_TRAITS = ['Openness', 'Conscientiousness', 'Extraversion', 'Agreeableness', 'Neuroticism']
+BIG5_TRAITS_SHORT = ['OPN', 'CON', 'EXT', 'AGR', 'NEU']
+BIG_TRAITS_LETTERS = list('OCEAN')
---
+++ inference.py
@@ -0,0 +1,24 @@
+import huggingface_hub
+import polars as pl
+from sentence_transformers import SentenceTransformer
+
+from globalvars import BIG5_TRAITS, LORA_MODEL
+
+# huggingface_hub.login()
+model = SentenceTransformer(LORA_MODEL)
+
+test_samples = [
+    "I love meeting new people and being the center of attention.",
+    "I often feel anxious and worry about small things.",
+    "I have a very short temper",
+    "I enjoy exploring abstract concepts and complex ideas.",
+    "I'm very opinionated and often debate with others",
+    "I prefer intimate spaces over large crowds"
+]
+
+embeddings = model.encode(test_samples)
+
+results_df = pl.DataFrame(embeddings, schema=BIG5_TRAITS)
+results_df = results_df.insert_column(0, pl.Series("text", test_samples))
+
+print(results_df)
